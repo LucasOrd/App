6931ef2b2cd9160157bcea14e75d736f36dc3556
Lucas Ordoñez <lordonez@technisys.com>
2021-12-17 14:12:38 -0300

    p

--- src/Constants/DataBase.js
+++ src/Constants/DataBase.js
@@ -1,3 +1,3 @@
 export const URL_API = "https://applucas-87bde-default-rtdb.firebaseio.com/";
 export const AUTH_URL =
-  "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=;
+  "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
--- src/Store/index.js
+++ src/Store/index.js
@@ -3,8 +3,11 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import CategoryReducer from "./Reducer/CategoryReducer";
 import thunk from "redux-thunk";
 
+import CategoryReducer from "./Reducer/AuthReducer";
+
 const RootReducer = combineReducers({
   categories: CategoryReducer,
+  auth: AuthReducer,
 });
 
 export default createStore(RootReducer, applyMiddleware(thunk));
