a4c0da6d23d8d034c26f22dc23d33b47437a26cf
Lucas Ordoñez <lordonez@technisys.com>
2021-12-20 12:17:23 -0300

    s

--- src/Constants/DataBase.js
+++ src/Constants/DataBase.js
@@ -2,4 +2,4 @@ export const URL_API = "https://applucas-87bde-default-rtdb.firebaseio.com/";
 export const URL_AUTH_API =
   "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD94_qS_a0zUUvEwCDkTTEeJAnpPuhbPOw";
 export const URL_LOGIN_API =
-  "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyD94_qS_a0zUUvEwCDkTTEeJAnpPuhbPOw";
+  "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD94_qS_a0zUUvEwCDkTTEeJAnpPuhbPOw";
